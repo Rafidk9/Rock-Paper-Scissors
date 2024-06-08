bf7c2567b89611ca76d578f71c7d9135c514c89f
Rafid Khan <111130331+Rafidk9@users.noreply.github.com>
2024-06-08 10:09:23 +0000

    changed bgColor of resultDisplay
    now shows different colors of background for different result cases of the game and did some minor formatting

--- script.js
+++ script.js
@@ -7,18 +7,11 @@ const btnClicks = {
 let totalPlayerScore = 0
 let totalCompScore = 0
 
-
-
 let compChoice = document.getElementById("compChoice");
 let playerChoice = document.getElementById("playerChoice");
 
-const buttons = document.getElementsByClassName("btn-container")
-
-const paper = document.getElementById("btnPaper")
-const scissors = document.getElementById("btnScissors")
- 
 function rockClick() {
-  const rock = document.getElementById("btnRock").textContent
+  const rock = document.getElementById("imgRock").textContent
   playerChoice.textContent = `Player: Rock`
   let randNum = Math.floor(Math.random() * 3) + 1
   let comp = btnClicks[randNum]
@@ -31,14 +24,14 @@ function rockClick() {
     totalCompScore += 1
     popUp("Computer Wins!")
   }
-  else{
+  else {
     popUp("It's a Tie!!")
   }
   document.getElementById('p0').textContent = `${totalPlayerScore}`
   document.getElementById('c0').textContent = `${totalCompScore}`
 }
 function paperClick() {
-  const paper = document.getElementById("btnPaper").textContent
+  const paper = document.getElementById("imgPaper").textContent
   playerChoice.textContent = `Player: Paper`
   let randNum = Math.floor(Math.random() * 3) + 1
   let comp = btnClicks[randNum]
@@ -51,14 +44,14 @@ function paperClick() {
     totalCompScore += 1
     popUp("Computer Wins!")
   }
-  else{
-    popUp("It's a Tie")
+  else {
+    popUp("It's a Tie!!")
   }
   document.getElementById('p0').textContent = `${totalPlayerScore}`
   document.getElementById('c0').textContent = `${totalCompScore}`
 }
 function scissorsClick() {
-  const scissors = document.getElementById("btnScissors").textContent
+  const scissors = document.getElementById("imgScissors").textContent
   playerChoice.textContent = `Player: Scissors`
   let randNum = Math.floor(Math.random() * 3) + 1
   let comp = btnClicks[randNum]
@@ -71,14 +64,26 @@ function scissorsClick() {
     totalCompScore += 1
     popUp("Computer Wins!")
   }
-  else{
-    popUp("It's a Tie!")
+  else {
+    popUp("It's a Tie!!")
   }
   document.getElementById('p0').textContent = `${totalPlayerScore}`
   document.getElementById('c0').textContent = `${totalCompScore}`
 }
 
-function popUp(message){
+function popUp(message) {
   const displayResult = document.getElementById("message")
   displayResult.textContent = message
+  if (message == "Computer Wins!") {
+    displayResult.classList.add("popUpLoseColor")
+    displayResult.classList.remove("popUpTieColor")
+  }
+  else if (message == "Player Wins!") {
+    displayResult.classList.add("popUpWinColor")
+    displayResult.classList.remove("popUpTieColor")
+    displayResult.classList.remove("popUpLoseColor")
+  }
+  else {
+    displayResult.classList.add("popUpTieColor")
+  }
 }
